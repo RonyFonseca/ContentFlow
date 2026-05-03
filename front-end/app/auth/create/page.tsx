c4a0617883723ee449822104dd51b9a8d349e240
Jamilly-Pedrosa <jamillypedrosa.contato@gmail.com>
2026-05-03 11:46:20 -0300

    feat(auth/create): adiciona cadastro de usuários

--- front-end/app/auth/create/page.tsx
+++ front-end/app/auth/create/page.tsx
@@ -1,8 +1,45 @@
 "use client";
+import Style from "../auth.module.css";
+import Logo from "../../../public/LogoTipo.svg";
+import AuthLayout from "../../components/AuthLayout/layout";
+import Image from "next/image";
+import { ArrowLeftIcon, EnvelopeIcon, LockKeyIcon, UserIcon } from "@phosphor-icons/react";
+
 export default function CreateAccountPage() {
     return (
-        <div className="flex items-center justify-center h-screen">
-            <h1 className="text-4xl font-bold text-gray-800">Create Account Page</h1>
-        </div>
+        <AuthLayout>
+            <header className={Style.header}>
+                <ul>
+                    <li><Image src={Logo} alt="Logo" width={160}/></li>
+                    <li className={Style.button_destaque_header}>
+                        <ArrowLeftIcon width={20}/>
+                        <a href="/auth/login">Log in</a>
+                    </li>
+                </ul>
+            </header>
+
+            <h2 className={Style.title}>Crie sua conta</h2>
+            <p className={Style.legend}>
+                Planeje. Organize. Publique. Cresça
+            </p>
+
+            <form className={Style.form}>
+                <div>
+                    <UserIcon width={20} className={Style.icon}/>
+                    <input type="text" placeholder="Nome" />
+                </div>
+
+                <div>
+                    <EnvelopeIcon width={20} className={Style.icon}/>
+                    <input type="email" placeholder="Email" />
+                </div>
+                <div>
+                    <LockKeyIcon width={20} className={Style.icon}/>
+                    <input type="password" placeholder="Senha" />
+                </div>
+
+                <button type="submit">Criar conta</button>
+            </form>
+        </AuthLayout>
     );
 }
