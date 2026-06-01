f0c84b583962a5cad08dd9e1b7eb32c7781c2fa2
Jamilly-Pedrosa <jamillypedrosa.contato@gmail.com>
2026-06-01 14:35:43 -0300

    feat(api): configura conexão com banco de dados

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -1,16 +1,19 @@
 import express from 'express'; 
+import cors from 'cors'; 
 import userRouter from './Router/userRouter.js';
-import postRouter from './Router/postRouter.js';
-import dashboardRouter from './Router/dashboardRouter.js';
+import postRouter from './Router/postRouter.js'; 
+import dashboardRouter from './Router/dashboardRouter.js'; 
 import dotenv from 'dotenv';
 
 dotenv.config();
 const app = express();
-app.use(express.json());
-app.use(userRouter);
-app.use(postRouter);
-app.use(dashboardRouter);
 
+app.use(cors()); 
+app.use(express.json());
+
+app.use('/api', userRouter);
+app.use('/api', postRouter);
+app.use('/api', dashboardRouter);
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
