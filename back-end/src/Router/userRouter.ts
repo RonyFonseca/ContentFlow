7f3380f1f2e19ba7d52795f51e3587996643ab0d
Jamilly-Pedrosa <jamillypedrosa.contato@gmail.com>
2026-07-02 15:56:20 -0300

    fix(userRouter): corrigi importação da rota

--- back-end/src/Router/userRouter.ts
+++ back-end/src/Router/userRouter.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import userController from '../controller/userController.js';
+import userController from '../Controller/userController.js';
 
 const router = express.Router();
 const userControllerInstance = new userController();
